2c8f32fc4bb032075cc79bfe3beb4b7e78e24810
EliezerSunny <adetunjieliazer@gmail.com>
2024-03-28 18:13:39 +0100

    username

--- app_username.js
+++ app_username.js
@@ -20,9 +20,9 @@ $(document).ready(function () {
             usernameError = false; 
             return false; 
         }
-	    /*else if (usernameValue === Evil || usernameValue === evil) {                                     $("#usercheck").show();                           $("#usercheck").html("Username already taken");
+	    else if (usernameValue === Evil || usernameValue === evil) {                                     $("#usercheck").show();                           $("#usercheck").html("Username already taken");
             usernameError = false;                            return false;
-        }*/
+        }
 	    else { 
             $("#usercheck").hide(); 
         } 
